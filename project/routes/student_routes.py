83ddd4612412882d1f14a9fe9ab08d439d571e51
jagadeesh <jagadeesh.vinnakota93@gmail.com>
2019-04-13 18:55:06 -0400

    Added get api routes
    Added api routes for student api

--- project/routes/student_routes.py
+++ project/routes/student_routes.py
@@ -1,6 +1,31 @@
 from project import app, services
-from flask import jsonify
+from flask import jsonify, request
 
+# Api route for gathering all students information
 @app.route("/v1/students", methods=['GET'])
 def student_list():
-    return jsonify(services.all_students())
+    students_data, decider = services.all_students()
+    if not decider:
+        return jsonify(students_data)
+    return jsonify(students_data)
+
+
+# Api route for retrieving single student information. This api route is using path parameters
+@app.route("/v1/students/<int:student_id>", methods=['GET'])
+def single_student(student_id):
+    students_data, decider = services.single_student(student_id)
+    if not decider:
+        return jsonify(students_data), 404
+    return jsonify(students_data)
+
+# Api route for filtering students information by gender. This api route is using query parameters
+@app.route("/v1/student/gender", methods=['GET'])
+def student_by_gender():
+    student_gender = request.args.get('gender')
+    if student_gender:
+        students_data, decider = services.students_by_gender(student_gender)
+        if not decider:
+            return jsonify(students_data), 200
+        return jsonify(students_data), 200
+    else:
+        return jsonify({"error": "Please Provide gender value <gender>"})
--- project/services/student_services.py
+++ project/services/student_services.py
@@ -1,22 +1,62 @@
 import pymysql
+import json
+
+
 def database_connection():
-    return pymysql.connect(host='localhost',
-                                 user='root',
-                                 password='root',
-                                 db='college')
-def all_students():
-    student_list = []
+    return pymysql.connect(host='localhost', user='root', password='root', db='college')
+
+
+def retrieve_data(sql_query, parameters):
     table_columns = []
-    sql_query = "select * from students"
     try:
         connection = database_connection()
         with connection.cursor() as cursor:
-            cursor.execute(sql_query)
+            cursor.execute(sql_query, parameters)
             for data in cursor.description:
                 table_columns.append(data[0])
             result = cursor.fetchall()
-    finally:
         connection.close()
+    except Exception as e:
+        return {"error": f"Something went wrong while retrieving data from data base. Please check query. {e.__str__()}"}, False
+    return result, table_columns
+
+
+# Function for retrieving all the students info from data base
+def all_students():
+    student_list = []
+    sql_query = "select * from students"
+    result, table_columns = retrieve_data(sql_query, [])
     for data in result:
         student_list.append(dict(zip(tuple(table_columns), data)))
-    return student_list
+    return student_list, True
+
+
+# Function for retrieving single student information based on ID of the student
+def single_student(student_id):
+    student_list = {}
+    params_list = []
+    params_list.append(int(student_id))
+    sql_query = "SELECT * FROM `students` WHERE `id`=%s"
+    result, table_columns = retrieve_data(sql_query, params_list)
+    for data in result:
+        student_list = dict(zip(tuple(table_columns), data))
+        break
+    if student_list:
+        return student_list, True
+    else:
+        return {}, False
+
+
+# Function for filtering students information based on gender
+def students_by_gender(gender):
+    student_list = []
+    params_list = []
+    params_list.append(gender)
+    sql_query = "SELECT * FROM `students` WHERE `gender`=%s"
+    result, table_columns = retrieve_data(sql_query, params_list)
+    for data in result:
+        student_list.append(dict(zip(tuple(table_columns), data)))
+    if student_list:
+        return student_list, True
+    else:
+        return [], False
